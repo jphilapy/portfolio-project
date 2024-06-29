f52e935354662794c57858ad999dd4644f25799b
Jeff Philapy <jeff@altahost.com>
2024-06-29 11:16:18 -0400

    misc

--- res/sqlite/users-seeder.sql
+++ res/sqlite/users-seeder.sql
@@ -1,10 +1,10 @@
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `is_active`, `created_at`)
-VALUES (1,'testuser1', 'testuser1@mydomain.com', 'aadadadwdw', 1, '2024-05-09 15:13:36'),
-       (2,'testuser2', 'testuser2@mydomain.com', 'adad', 0, '2024-05-09 21:05:01'),
-       (5,'testuser3', 'testuser3@mydomain.com', '2222', 0, '2024-05-09 23:13:29'),
-       (6,'testuser4', 'testuser4@mydomain.com', '22', 0, '2024-05-09 23:13:36'),
-       (7,'testuser5', 'testuser5@mydomain.com', '33', 0, '2024-05-09 23:13:42'),
-       (8,'testuser6', 'testuser6@mydomain.com', '44', 0, '2024-05-09 23:13:49'),
-       (9,'testuser7', 'testuser7@mydomain.com', '55', 0, '2024-05-09 23:13:54'),
-       (10,'testuser8', 'testuser8@mydomain.com', '66', 0, '2024-05-09 23:13:59'),
-       (11,'testuser9', 'testuser9@mydomain.com', '333333', 0, '2024-05-09 23:14:09');
+VALUES (1,'testuser1', 'testuser1@mydomain.com', '$2y$10$Fm8r67HlVQwp64TdW2xp.Ob3pavETitik4ROS89FwtoBj97udw6jq', 1, '2024-05-09 15:13:36'),
+       (2,'testuser2', 'testuser2@mydomain.com', '$2y$10$Fm8r67HlVQwp64TdW2xp.Ob3pavETitik4ROS89FwtoBj97udw6jq', 0, '2024-05-09 21:05:01'),
+       (5,'testuser3', 'testuser3@mydomain.com', '$2y$10$Fm8r67HlVQwp64TdW2xp.Ob3pavETitik4ROS89FwtoBj97udw6jq', 0, '2024-05-09 23:13:29'),
+       (6,'testuser4', 'testuser4@mydomain.com', '$2y$10$Fm8r67HlVQwp64TdW2xp.Ob3pavETitik4ROS89FwtoBj97udw6jq', 0, '2024-05-09 23:13:36'),
+       (7,'testuser5', 'testuser5@mydomain.com', '$2y$10$Fm8r67HlVQwp64TdW2xp.Ob3pavETitik4ROS89FwtoBj97udw6jq', 0, '2024-05-09 23:13:42'),
+       (8,'testuser6', 'testuser6@mydomain.com', '$2y$10$Fm8r67HlVQwp64TdW2xp.Ob3pavETitik4ROS89FwtoBj97udw6jq', 0, '2024-05-09 23:13:49'),
+       (9,'testuser7', 'testuser7@mydomain.com', '$2y$10$Fm8r67HlVQwp64TdW2xp.Ob3pavETitik4ROS89FwtoBj97udw6jq', 0, '2024-05-09 23:13:54'),
+       (10,'testuser8', 'testuser8@mydomain.com', '$2y$10$Fm8r67HlVQwp64TdW2xp.Ob3pavETitik4ROS89FwtoBj97udw6jq', 0, '2024-05-09 23:13:59'),
+       (11,'testuser9', 'testuser9@mydomain.com', '$2y$10$Fm8r67HlVQwp64TdW2xp.Ob3pavETitik4ROS89FwtoBj97udw6jq', 0, '2024-05-09 23:14:09');
